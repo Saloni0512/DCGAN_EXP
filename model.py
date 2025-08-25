4e618a217f32ef29755551a395239f52dc042db4
Saloni0512 <sinceresaloni@gmail.com>
2025-08-25 13:51:32 +0530

    Added tests for checking tensor shape values✔️

--- model.py
+++ model.py
@@ -1,31 +1,54 @@
 import torch.nn as nn
 import config
+import torch
 
 class Generator(nn.Module):
   """The generator class module in dcgan for generating fake images."""
   def __init__(self, ngpu):
     super(Generator, self).__init__()
     self.ngpu = ngpu
-    self.main = nn.Sequential(
-        # Takes latent space vector NZ as input
+    
+    # Takes latent space vector NZ as input
+    self.block1 = nn.Sequential(
         nn.ConvTranspose2d(config.NZ, config.NGF * 4, 4, 1, 0, bias=False),
         nn.BatchNorm2d(config.NGF * 4),
         nn.ReLU(True),
-        # Upsamples to 8x8
+    )    
+    # Upsamples to 8x8
+    self.block2 = nn.Sequential(
         nn.ConvTranspose2d(config.NGF * 4, config.NGF * 2, 4, 2, 1, bias=False),
         nn.BatchNorm2d(config.NGF * 2),
         nn.ReLU(True),
-        # Upsamples to 16x16
+    )    
+    # Upsamples to 16x16
+    self.block3 = nn.Sequential(    
         nn.ConvTranspose2d(config.NGF * 2, config.NGF, 4, 2, 1, bias=False),
         nn.BatchNorm2d(config.NGF),
         nn.ReLU(True),
-        # Upsamples to 32x32
+    )    
+    # Upsamples to 32x32
+    self.block4 = nn.Sequential(    
         nn.ConvTranspose2d(config.NGF, config.NC, 4, 2, 1, bias=False),
         nn.Tanh()
     )
   
   def forward(self, input):
-    return self.main(input)
+    # Check intermediate tensor shapes after each block
+    x = self.block1(input)
+    print(f"After Block 1 (4x4): \t\t{x.shape}")
+    
+    x = self.block2(x)
+    print(f"After Block 2 (8x8): \t\t{x.shape}")
+    
+    x = self.block3(x)
+    print(f"After Block 3 (16x16): \t\t{x.shape}")
+    
+    x = self.block4(x)
+    print(f"After Block 4 (Output 32x32): \t{x.shape}")
+    
+    return x
+    
+    
 
 
 class Discriminator(nn.Module):
@@ -51,3 +74,16 @@ class Discriminator(nn.Module):
   
   def forward(self, input):
     return self.main(input)
+  
+
+
+if __name__ == '__main__':
+  # initialise generator 
+  gen = Generator(ngpu=config.NGPU).to(config.DEVICE)
+  print(f"Testing tensor shape values in Generator...")
+  # Create a random latent vector
+  noise = torch.randn(config.BATCH_SIZE, config.NZ, 1, 1, device=config.DEVICE)
+  fake_img = gen(noise)
+  print("-------------------------\n")
+
+
